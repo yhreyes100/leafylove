6a6eed3cf7474be0f4279b5e5ec4e94a5dd0cbf3
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-10-30 23:42:23 +0000

    commitsingup

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,39 +2,37 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/login.css";
 import loginpicImageUrl1 from "../../img/pl1.jpg";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [passw, setPassw] = useState("")
 	const [email, setEmail] = useState("")
 	return (
 		<section className="myform-area">
-			<div className="container">
-				<div className="row justify-content-center">
-					<div className="col-lg-8">
-						<div className="form-area login-form">
-							<div className="form-content">
-								<img src={loginpicImageUrl1} />
+			<div className="container justify-content-center">
+				<div className="row">
+					<div className="col-lg-2"></div>
+					<div className="col-lg-8 col-sm-12 row justify-content-center content">
+						<div className="col-lg-6 col-sm-6 content">
+							<img src={loginpicImageUrl1} />
+						</div>
+						<div className="col-lg-6 col-sm-6 content">
+							<h2 className="sitename">LeafyLove</h2>
+							<div className="form-floating form-group">
+								<input type="email" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
+								<label className="glabel" htmlFor="floatingInput">Email or Nickname</label>
 							</div>
-							<div className="form-input">
-								<h2>LeafyLove</h2>
-								<div className="col form">
-									<div className="form-floating">
-										<input type="email" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
-										<label className="glabel" htmlFor="floatingInput">Email or Nickname</label>
-									</div>
-									<div className="form-floating form-group">
-										<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
-										<label className="glabel" htmlFor="floatingPassword">Password</label>
-									</div>
-									<button className="btn btn-success rbuttom">Login</button>
-									<div className="text-center pb-4 mt-4">
-										Don't have an account?  <Link to="/singup">Singup</Link>
-									</div>
-								</div>
+							<div className="form-floating form-group">
+								<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
+								<label className="glabel" htmlFor="floatingPassword">Password</label>
+							</div>
+							<button className="btn btn-success rbuttom">Login</button>
+							<div className="text-center pb-4 mt-4">
+								You have an account? <Link to="/singup">Singup</Link>
 							</div>
 						</div>
 					</div>
+					<div className="col-lg-2"></div>
 				</div>
 			</div>
 		</section>
--- src/front/js/pages/singup.js
+++ src/front/js/pages/singup.js
@@ -10,36 +10,38 @@ export const Singup = () => {
 	const [name, setName] = useState("")
 	return (
 		<section className="myform-area">
-			<div className="container">
-				<div className="row justify-content-center">
-					<div className="col-lg-8">
-						<div className="form-area login-form">
-							<div className="form-content">
-								<img src={loginpicImageUrl1} />
+			<div className="container justify-content-center">
+				<div className="row">
+					<div className="col-lg-2"></div>
+					<div className="col-lg-8 col-sm-12 row justify-content-center content">
+						<div className="col-lg-6 col-sm-6 content">
+							<img src={loginpicImageUrl1} />
+						</div>
+						<div className="col-lg-6 col-sm-6 content">
+							<h2 className="sitename">LeafyLove</h2>
+							<div className="form-floating form-group">
+								<input type="text" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
+								<label className="glabel" htmlFor="floatingInput">Nickname</label>
 							</div>
-							<div className="form-input">
-								<h2>LeafyLove</h2>
-								<div className="col form">
-									<div className="form-floating ">
-										<input type="text" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingName" placeholder="name@example.com" required />
-										<label className="glabel" htmlFor="floatingName">Nickname</label>
-									</div>
-									<div className="form-floating">
-										<input type="email" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
-										<label className="glabel" htmlFor="floatingInput">Email</label>
-									</div>
-									<div className="form-floating form-group">
-										<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
-										<label className="glabel" htmlFor="floatingPassword">Password</label>
-									</div>
-									<button className="btn btn-success rbuttom">Singup</button>
-									<div className="text-center pb-4 mt-4">
-										You have an account? <Link to="/login">Login</Link>
-									</div>
-								</div>
+							<div className="form-floating form-group">
+								<input type="email" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
+								<label className="glabel" htmlFor="floatingInput">Email</label>
+							</div>
+							<div className="form-floating form-group">
+								<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
+								<label className="glabel" htmlFor="floatingPassword">Password</label>
+							</div>
+							<div className="form-floating form-group">
+								<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
+								<label className="glabel" htmlFor="floatingPassword">Repeat Password</label>
+							</div>							
+							<button className="btn btn-success rbuttom">Singup</button>
+							<div className="text-center pb-4 mt-4">
+								You have an account? <Link to="/login">Login</Link>
 							</div>
 						</div>
 					</div>
+					<div className="col-lg-2"></div>
 				</div>
 			</div>
 		</section>
