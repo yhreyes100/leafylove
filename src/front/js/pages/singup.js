a9c942cdc4e71d8015eb118a773f00a808d579fc
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-10-30 22:12:13 +0000

    commitsingup

--- src/front/js/pages/singup.js
+++ src/front/js/pages/singup.js
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/login.css";
+import "../../styles/singup.css";
 import loginpicImageUrl1 from "../../img/pl1.jpg";
 import { useNavigate,Link } from "react-router-dom";
-export const Login = () => {
+export const Singup = () => {
 	const { store, actions } = useContext(Context);
 	const [passw, setPassw] = useState("")
 	const [email, setEmail] = useState("")
+	const [name, setName] = useState("")
 	return (
 		<section className="myform-area">
 			<div className="container">
@@ -19,17 +20,21 @@ export const Login = () => {
 							<div className="form-input">
 								<h2>LeafyLove</h2>
 								<div className="col form">
+									<div className="form-floating ">
+										<input type="text" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingName" placeholder="name@example.com" required />
+										<label className="glabel" htmlFor="floatingName">Nickname</label>
+									</div>
 									<div className="form-floating">
 										<input type="email" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingInput" placeholder="name@example.com" required />
-										<label className="glabel" htmlFor="floatingInput">Email or Nickname</label>
+										<label className="glabel" htmlFor="floatingInput">Email</label>
 									</div>
 									<div className="form-floating form-group">
 										<input type="password" className="form-control border-0 border-bottom border-success shadow-none text-success fw-bold ginput" id="floatingPassword" placeholder="Password" required />
 										<label className="glabel" htmlFor="floatingPassword">Password</label>
 									</div>
-									<button className="btn btn-success rbuttom">Login</button>
+									<button className="btn btn-success rbuttom">Singup</button>
 									<div className="text-center pb-4 mt-4">
-										Don't have an account?  <Link to="/singup">Singup</Link>
+										You have an account? <Link to="/">Login</Link>
 									</div>
 								</div>
 							</div>
