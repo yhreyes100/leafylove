336913f8233ed258e045c2c3ac31c8308df337c2
Anish-tech123 <rvermaji321@alumni.iastate.edu>
2024-11-02 21:54:33 +0000

    added more features

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, unique=False, nullable=False)
     firstname = db.Column(db.String(250), nullable=False)
     lastname = db.Column(db.String(250), nullable=False)
 
@@ -20,4 +20,24 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
     
-    # class Plants()
+class Plants(db.Model):
+   # __tablename__="plant_table"
+    id=db.Column(db.Integer,primary_key=True)
+    common_name=db.Column(db.String(250),nullable=False)
+    scientific_name=db.Column(db.String(250),nullable=True)
+    other_name=db.Column(db.String(250),nullable=True)
+    default_image=db.Column(db.String(250),nullable=True)
+    watering=db.Column(db.String(250),nullable=True)
+
+    def __repr__(self):
+        return f'<Plants {self.common_name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "common_name": self.common_name,
+            "scientefic__name": self.scientefic_name,
+            "other_name":self.other_name,
+            "default_image":self.default_image,
+            "watering":self.watering,
+        }
---
+++ src/front/js/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlantBlog = () => {
+    return (
+        <div>
+            <h2>Plant Blog TBC</h2>
+        </div>
+    )
+}
+
+export default PlantBlog;
