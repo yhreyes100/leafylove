c73af3f9ff960ae33304b23e7d79bf5b8f7272d7
Daniel Montoya <153567770+mannydontoya@users.noreply.github.com>
2024-11-06 23:51:58 +0000

    Favorite feature

--- src/front/js/pages/library.js
+++ src/front/js/pages/library.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 
 export const Library = () => {
-	const { store, actions } = useContext(Context);
-    useEffect(() =>{
-        actions.getPlantList()
-    }, [])
-	return (
-		<div className="container d-flex gap-5">
-            
-			{
-                store.plantList.slice(0, 10).map((plant) =>{
-                    return <div key={plant.id}>
-                        <h1>{plant.common_name}</h1>
-                        <h2>id: {plant.id}</h2>
-                    </div>
-                })
-            }
-		</div>
-	);
+    const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        actions.getPlantList();
+    }, []);
+
+    return (
+        <div className="container d-flex gap-5">
+            {store.plantList.slice(0, 10).map((plant) => (
+                <div key={plant.id}>
+                    <h1>{plant.common_name}</h1>
+                    <h2>id: {plant.id}</h2>
+                    <button onClick={() => actions.addFavorite(plant)}>Add to Favorites</button>
+                    <button onClick={() => actions.removeFavorite(plant.id)}>Remove from Favorites</button>
+                </div>
+            ))}
+        </div>
+    );
 };
