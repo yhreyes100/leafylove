7fe8502f291f3288b773b5fc0fb60a5136a5535a
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-11-13 23:47:50 +0000

    signup-and-login

---
+++ src/front/js/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/login.css";
+import { useNavigate, Link } from "react-router-dom";
+import validator from "validator";
+export const Dashboard = () => {
+	const { store, actions } = useContext(Context);
+	
+	return (
+		<>
+        
+        </>
+	);
+};
